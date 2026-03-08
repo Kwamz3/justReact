7ae874ce4d120d7d17cd8e2acbd1114b3efd5116
Kwamz3 <charlie18appiah@gmail.com>
2026-03-08 01:50:47 +0000

    feat: add Food component displaying a list of formatted food items.

--- Bro code/test-react/src/Food.jsx
+++ Bro code/test-react/src/Food.jsx
@@ -3,10 +3,13 @@ function Food() {
 
     const food1 = "Pizza";
     const food2 = "Burger";
+    const food3 = "Pasta";
+    const food4 = "Spaghetti";
 
     return (
         <ul>
-            <li>Apple</li>
+            <li>{food4.toLowerCase()}</li>
+            <li>{food3.toUpperCase()}</li>
             <li>{food2.toLowerCase()}</li>
             <li>{food1.toUpperCase()}</li>
         </ul>
