12dde714775a640165480562c9bec5de8125277c
Kwamz3 <charlie18appiah@gmail.com>
2026-03-12 13:24:22 +0000

    refactor: update title in index.html and simplify App component

---
+++ practReact/movieTutorial/movie_frontend/src/components/movieCard.jsx
@@ -0,0 +1,20 @@
+
+function MovieCard({movie}) {
+    
+    function onFavouriteClick() {
+        alert('clicked')
+    }
+
+    return <div className= "movie-card">
+        <div className= "movie-poster">
+            <img src={movie.url} alt={movie.title} />
+        </div>
+        <div className= "movie-overlay">
+            <button className="favourite-btn" onClick= {onFavouriteClick}>
+                    
+            </button>
+        </div>
+    </div>
+}
+
+export default MovieCard
